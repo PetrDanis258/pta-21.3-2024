8bfaddb06647ecb022c0df8f6126ffbfe50100a6
22ib03_DANIS <22ib03_DANIS@U1-02>
2024-03-21 10:43:55 +0100

    1

--- pta-21.3 2024/MainWindow.xaml.cs
+++ pta-21.3 2024/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,54 @@ namespace pta_21._3_2024
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void OPN_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog OD = new OpenFileDialog()
+            {
+                Filter = "txt soubory (*.txt)|*.txt",
+                Title = "otevřít TXT soubor"
+            };
+            if (OD.ShowDialog() == true)
+            {
+                path = OD.FileName;
+                btnwrite.IsEnabled = true;
+
+                precit(path);
+            }
+        }
+
+        private void precit(string path)
+        {
+            textvystup.Text = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string radek;
+                while ((radek = sr.ReadLine()) != null)
+                {
+                    textvystup.Text += radek + "\n";
+                }
+            }
+        }
+
+        private void WRT_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string vstup = textvstup.Text;
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.Write($"{vstup}({DateTime.Now.ToLocalTime()})\n");
+            }
+            textvstup.Text = "";
+            status.Fill = Brushes.Green;
+            precit(path);
+        }
+        private void textvstup_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            status.Fill = Brushes.Red;
         }
     }
 }
